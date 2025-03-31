1e3235cb40517168af7cd14589c31275108adfb5
Aadesh Puthran <puthranaadesh3@gmail.com>
2025-03-31 19:14:28 +0530

    Added Suspense boundary over <Gemini> for useSearchParams()

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -4,19 +4,18 @@ import ModelsDialog from "@/components/ModelsDialog";
 import { Suspense } from "react";
 
 const Layout = async ({ children }: { children: React.ReactNode }) => {
-
   return (
-    <Gemini>
-      <main className="flex min-h-screen w-full flex-col lg:flex-row">
-        <Suspense>
+    <Suspense>
+      <Gemini>
+        <main className="flex min-h-screen w-full flex-col lg:flex-row">
           <Sidebar />
-        </Suspense>
-        <div className="mt-3 mb-[10px] ml-[10px]">
-          <ModelsDialog />
-        </div>
-        {children}
-      </main>
-    </Gemini>
+          <div className="mt-3 mb-[10px] ml-[10px]">
+            <ModelsDialog />
+          </div>
+          {children}
+        </main>
+      </Gemini>
+    </Suspense>
   );
 };
 
